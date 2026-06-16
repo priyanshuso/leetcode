99c1a183c190c7c0c7182f44ee941d0cea8a6882
priyanshu <priyanshu8115soni@gmail.com>
2026-06-16 21:06:51 +0530

    Added LeetCode solutions

---
+++ problem no. 922.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int even = 0;
+        int odd = 1;
+
+        while (even < nums.size() && odd < nums.size()) {
+            if (nums[even] % 2 == 0) {
+                even += 2;
+
+            } else if (nums[odd] % 2 == 1) {
+                odd += 2;
+
+            }
+
+            else {
+                swap(nums[even], nums[odd]);
+
+                odd += 2;
+                even += 2;
+            }
+        }
+
+        return nums;
+    }
+};
