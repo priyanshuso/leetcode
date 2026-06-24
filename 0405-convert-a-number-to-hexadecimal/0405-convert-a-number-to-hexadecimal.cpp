d47d352e53747cc94e063707ba8bb81f8d028eff
Priyanshu Soni <priyanshu8115soni@gmail.com>
2026-06-24 19:54:47 +0530

    Time: 0 ms (100%), Space: 7.9 MB (10.24%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+
+        string hex = "0123456789abcdef";
+        string ans = "";
+
+        unsigned int n = num;
+
+        while (n > 0) {
+            ans = hex[n % 16] + ans;
+            n /= 16;
+        }
+
+        return ans;
+    }
+};
